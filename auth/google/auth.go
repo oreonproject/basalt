31d4e3ef5577c597b70bc14befe0070fe7dd0c1f
Pancaakeman <haxionrising@gmail.com>
2025-11-17 21:33:04 +0000

    Added a secure code verification gen for Google oAuth

---
+++ auth/google/auth.go
@@ -0,0 +1,26 @@
+package google
+
+import (
+	crand "crypto/rand"
+	"crypto/sha256"
+	"log"
+	"oreonproject/basalt/utils"
+)
+
+func CodeVerifyKeyGen() []byte {
+	// We will Initialise the logs
+	utils.LogInit("logs/auth.log")
+
+	key := make([]byte, 32) // creates a new byte slice
+
+	// Use the Read Function from crypto/rand to populate the key with cryptographically secure values
+	crand.Read(key)
+	log.Print("Populated the Key")
+
+	// Initialise a hasher
+	hasher := sha256.New()
+	// Write to the key using the hash digest for key
+	hasher.Write([]byte(key))
+	log.Println("Code Verification Key Generated")
+	return key
+}
