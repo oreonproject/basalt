8f9d6e5d4b2ce6f70195906f3289da25c71d0cc9
Ali moumen <moumenisbest@gmail.com>
2025-11-18 19:27:26 +0100

    added Google OAuth and a basic example for it

--- auth/google/auth.go
+++ auth/google/auth.go
@@ -1,26 +1,59 @@
 package google
 
+
 import (
-	crand "crypto/rand"
-	"crypto/sha256"
+	"context"
+	"encoding/json"
+	"fmt"
 	"log"
-	"oreonproject/basalt/utils"
+	"net/http"
+	"os"
+	"golang.org/x/oauth2"
 )
 
-func CodeVerifyKeyGen() []byte {
-	// We will Initialise the logs
-	utils.LogInit("logs/auth.log")
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err 
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err 
+}
 
-	CodeVerifierKey := make([]byte, 32) // creates a new byte slice
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to %s\n", path)
+	f, err := os.Create(path)
+	if err != nil {
+		log.Fatalf("Unable to cache OAuth token: %v", err)
+		
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
 
-	// Use the Read Function from crypto/rand to populate the CodeVerifierKey with cryptographically secure random values
-	crand.Read(CodeVerifierKey)
-	log.Print("Populated the Key")
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	    fmt.Printf("Go to the following link in your browser then type the code: \n%v\n", authURL)
+	    var code string 
+	    if _, err := fmt.Scan(&code); err != nil {
+		    log.Fatalf("Unable to read code: %v", err)
+	    }
+	    tok, err := config.Exchange(context.Background(), code)
+	    if err != nil {
+		    log.Fatalf("Unable to retreive token from web: %v", err)
+	    }
+	    return tok
+}
 
-	// Initialise a hasher
-	hasher := sha256.New()
-	// Write to the CodeVerifierKey using the hash digest for CodeVerifierKey as an extra safeguard
-	hasher.Write(CodeVerifierKey)
-	log.Println("Code Verification Key Generated")
-	return CodeVerifierKey
+
+func GetClient(config *oauth2.Config) *http.Client {
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
 }
--- main.go
+++ main.go
@@ -2,13 +2,37 @@ package main
 
 import (
 	"log"
-	"oreonproject/basalt/cmd"
-	"oreonproject/basalt/utils"
+	"os"
+	"google.golang.org/api/drive/v3"
+	"golang.org/x/oauth2/google"
+        authGoogle "oreonproject/basalt/auth/google"
 )
 
 func main() {
-	utils.LogInit("logs/main.log")
-	log.Print("Logger was Setup")
-	cmd.Execute()
-	log.Print("Executed root command")
+	b, err := os.ReadFile("credentials.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+	config, err := google.ConfigFromJSON(b, drive.DriveReadonlyScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v")
+	}
+	client := authGoogle.GetClient(config)
+	srv, err := drive.New(client)
+	if err != nil {
+		log.Fatalf("Unable to retreive Drive client: %v", err)
+	}
+	r, err := srv.Files.List().PageSize(10).Fields("files(id, name)").Do()
+	if err != nil {
+		log.Fatalf("Unable to retreive files: %v", err)
+	}
+	if len(r.Files) == 0 {
+		log.Println("No lines were found.")
+	} else {
+		log.Println("Files:")
+		for _, i := range r.Files  {
+			log.Printf("%s (%s)\n", i.Name, i.Id)
+		}
+	}
+	
 }
