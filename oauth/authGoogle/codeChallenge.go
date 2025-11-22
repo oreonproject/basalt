6ead0c2ffb2b3588cc5ff5d2811d68c7fd160284
Pancaakeman <haxionrising@gmail.com>
2025-11-22 15:27:24 +0000

    Added state token Generation

--- oauth/authGoogle/codeChallenge.go
+++ oauth/authGoogle/codeChallenge.go
@@ -1,34 +1,41 @@
-package google
+package authGoogle
 
 import (
 	crand "crypto/rand"
 	"crypto/sha256"
 	"encoding/base64"
-	"fmt"
 	"log"
 	"oreonproject/basalt/utils"
 )
 
 func CodeVerifierKeyGen() []byte {
 	// We will Initialise the logs
-	utils.LogInit("logs/auth.log")
+	utils.LogInit("logs/oauth.log")
 	codeVerifierKey := make([]byte, 32) // creates a new byte slice
-	log.Print("PreSaltKey Initialised")
 
 	// Use the Read Function from crypto/rand to populate the codeVerifier with cryptographically secure random values
 	crand.Read(codeVerifierKey)
-	log.Print("Populated the PreSaltKey")
 	log.Println("Code Verification Key Generated")
 
 	return codeVerifierKey
 }
 
-func CodeChallengeGen(codeVerifier []byte) {
+func CodeChallengeGen(codeVerifier []byte) string {
+	utils.LogInit("logs/oauth.log")
 	var codeChallenge string
 
 	hasher := sha256.New()
 	hasher.Write(codeVerifier) // Hashes the codeVerifier
 
 	codeChallenge = base64.RawURLEncoding.EncodeToString(codeVerifier) // Encodes the base64 URL to a string
-	fmt.Println(codeChallenge)
+	log.Print("codeChallenge Generated")
+	return codeChallenge
+}
+
+func StateTokGen() string {
+	stateTok := make([]byte, 16)
+	crand.Read(stateTok) // Populates State Token
+
+	state := base64.RawURLEncoding.EncodeToString(stateTok) // Encodes state to base64 for added protections
+	return state
 }
